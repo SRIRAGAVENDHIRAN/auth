c282c17f8c8b3d79c39c7cbc8503c2331ebc39e0
Sriragavendhiran <ragavenragaven06@gmail.com>
2025-03-29 14:01:38 +0530

    backend change

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: "https://auth-frontend-zmpm.onrender.com", credentials: true }));
 app.use(cookieParser());
 
 
