8785d893e081a522f25202bf7a54ade4a8a2e6b4
Sriragavendhiran <ragavenragaven06@gmail.com>
2025-03-29 16:17:54 +0530

    back

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: ["https://auth-frontend-bm1x.onrender.com","http://localhost:3000"], credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(cookieParser());
 
 
