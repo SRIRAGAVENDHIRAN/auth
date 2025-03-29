527180a3902d808510da0bf78ce3faedec0d22ef
Sriragavendhiran <ragavenragaven06@gmail.com>
2025-03-29 15:14:58 +0530

    final commit

--- server.js
+++ server.js
@@ -4,34 +4,28 @@ import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoutes from './routes/auth.js';
-import path from 'path';
-import { fileURLToPath } from "url";
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: "https://auth-frontend-zmpm.onrender.com", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(cookieParser());
+
+
 
 // MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log("MongoDB Connection Error:", err));
+  .catch((err) => console.log("MongoDB Connection Error:",err));
 
-// Routes
+app.get("/", (req, res) => res.send("API is running..."));
+
+//Routes
 app.use('/api/auth', authRoutes);
-
-// Serve Frontend
-app.use(express.static(path.join(__dirname, "build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
